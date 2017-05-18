ae6c09fb1e07e4a2613f987d262f94f745921541
Ali Mohammadlo <netcat.av@gmail.com>
2017-05-18 16:40:01 +0430

    Drop react-native-communications package

--- src/components/advance/chat/MessageText.js
+++ src/components/advance/chat/MessageText.js
@@ -8,41 +8,16 @@ import {
 } from 'react-native';
 
 import ParsedText from 'react-native-parsed-text';
-import Communications from 'react-native-communications';
 
 export default class MessageText extends React.Component {
   constructor(props) {
     super(props);
     this.onUrlPress = this.onUrlPress.bind(this);
-    this.onPhonePress = this.onPhonePress.bind(this);
     this.onEmailPress = this.onEmailPress.bind(this);
   }
 
   onUrlPress(url) {
     Linking.openURL(url);
-  }
-
-  onPhonePress(phone) {
-    const options = [
-      'Text',
-      'Call',
-      'Cancel',
-    ];
-    const cancelButtonIndex = options.length - 1;
-    this.context.actionSheet().showActionSheetWithOptions({
-      options,
-      cancelButtonIndex,
-    },
-    (buttonIndex) => {
-      switch (buttonIndex) {
-        case 0:
-          Communications.phonecall(phone, true);
-          break;
-        case 1:
-          Communications.text(phone);
-          break;
-      }
-    });
   }
 
   onEmailPress(email) {
@@ -56,7 +31,6 @@ export default class MessageText extends React.Component {
           style={[styles[this.props.position].text, this.props.textStyle[this.props.position]]}
           parse={[
             {type: 'url', style: StyleSheet.flatten([styles[this.props.position].link, this.props.linkStyle[this.props.position]]), onPress: this.onUrlPress},
-            {type: 'phone', style: StyleSheet.flatten([styles[this.props.position].link, this.props.linkStyle[this.props.position]]), onPress: this.onPhonePress},
             {type: 'email', style: StyleSheet.flatten([styles[this.props.position].link, this.props.linkStyle[this.props.position]]), onPress: this.onEmailPress},
           ]}
         >
