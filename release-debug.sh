c07fcaaf79ac5e2d13b9674b7bf3a11972ef391d
N3TC4T <netcat.av@gmail.com>
2017-11-22 23:57:20 +0330

    Improved

--- release-debug.sh
+++ release-debug.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+echo -e "[!] Creating Release APK ... \n"
+
 cd android && ./gradlew assembleRelease
-adb -s 'F7AZFG100813' install -r app/build/outputs/apk/app-release.apk
+
+if hash adb 2>/dev/null; then
+    echo -e "\n[!] Installing on android devices with ADB ..."
+    for line in `adb devices | grep -v "List"  | awk '{print $1}'`
+    do
+      device=`echo $line | awk '{print $1}'`
+      echo -e "[+] trying to install on device $device ..."
+      adb -s $device install -r app/build/outputs/apk/app-debug.apk
+    done
+else
+    echo "\n[!] ADB is not installed , please install it and try again ."
+fi
